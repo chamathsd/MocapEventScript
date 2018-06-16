536cb086c21e116e85e9c7777ea27697407ca924
Jake Dharmasiri <jschadaway@gmail.com>
2018-06-15 20:02:54 -0500

    Initial commit

---
+++ detect_events.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Author: Chamath "Jake" Dharmasiri
+Date: 06/15/2018
+
+Script to automate the detection of motion events between predetermined nodes
+for the purpose of mocap data analysis.
+
+This script takes as its only command-line argument the relative or absolute
+path to the exported TSV file of the mocap data. From there, the user will be
+asked to specify the nodes that define the motion event.
+"""
+
