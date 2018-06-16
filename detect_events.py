f1e7dd4c414c5cb3a47b3bf8e8dfbf250d08c5de
Jake Dharmasiri <jschadaway@gmail.com>
2018-06-15 21:14:29 -0500

    Rename tsv to data, initial logic for fetching file off command line

--- detect_events.py
+++ detect_events.py
@@ -13,3 +13,20 @@ path to the exported TSV file of the mocap data. From there, the user will be
 asked to specify the nodes that define the motion event.
 """
 
+
+import sys, csv
+
+# Check for proper amount of command-line arguments
+if not len(sys.argv) == 2:
+    print("Wrong amount of command line arguments specified. Script takes one"
+          " TSV file as input.")
+    sys.exit()
+
+# Begin load of TSV document
+try:
+    with open(sys.argv[1]) as tsv:
+        reader = csv.DictReader(tsv, delimiter='\t')
+
+# If the file does not exist
+except FileNotFoundError:
+    print("Couldn't find matching file for '" + str(sys.argv[1]) + "'")
